6331b7a806cd3f30f1f0f6325dac761bb5772592
John Stovin <john.d.stovin@gmail.com>
2016-05-19 16:54:39 +0100

    Problem 6

--- Program.fs
+++ Program.fs
@@ -17,6 +17,24 @@ fibs |> Seq.takeWhile(fun x -> x <= 4000000) |> Seq.filter (fun x -> x % 2 = 0) 
 
 
 // Problem 3
+let isPrime n =
+  let sqrt' = (float >> sqrt >> int) n // square root of integer
+  [ 2 .. sqrt' ] // all numbers from 2 to sqrt'
+  |> List.forall (fun x -> n % x <> 0) // no divisors
+
+let allPrimes =
+  let rec allPrimes' n =
+    seq { // sequences are lazy, so we can make them infinite
+      if isPrime n then
+        yield n
+      yield! allPrimes' (n+1) // recursing
+    }
+  allPrimes' 2 // starting from 2
+
+let factors (n : bigint) = 
+    allPrimes |> Seq.takeWhile (fun x -> x < int (sqrt (float n))) |> Seq.filter (fun x -> n % bigint x = 0I) |> List.ofSeq 
+
+factors 600851475143I |> List.last
 
 // Problem 4
 let reverse (s : string) = 
@@ -39,3 +57,16 @@ let rec allInts n = seq {
 
 allInts 1 |> Seq.find (isEvenlyDivisibleBy ([2..20] |> List.rev)) |> int
 
+// Problem 6
+let sumSquares l =
+    l |> List.map (fun x -> x * x) |> List.sum
+
+let squareSum l =
+    l |> List.sum |> (fun x -> x * x) 
+
+let nums = [1..100]
+    
+(nums |> squareSum) - (nums |> sumSquares) Seq.l
+
+// Problem 7 - see problem 3
+allPrimes |> Seq.take 10001 |> List.ofSeq |> List.last;;
