f2c64a41d85626093a8df28b346ab1efd90a29c4
John Stovin <john.d.stovin@gmail.com>
2016-05-19 07:55:33 +0100

    Exercise 2

--- Program.fs
+++ Program.fs
@@ -1,7 +1,17 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
-[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+// Problem 1
+[1..999] |> List.filter (fun x -> x % 3 = 0 || x % 5 = 0) |> List.sum
+
+
+// Problem 2
+let rec fib n =
+    match n with
+    | 0 -> 1
+    | 1 -> 1
+    | _ -> fib (n - 2) + fib (n - 1)
+
+let fibs = Seq.initInfinite (fun n -> fib n)
+fibs |> Seq.takeWhile(fun x -> x <= 4000000) |> Seq.filter (fun x -> x % 2 = 0) |> Seq.sum
+
